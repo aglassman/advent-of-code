b0d8410b72f1ac82511995541b416dd1720feea4
aglassman <aglassman@digitalmeasures.com>
2019-12-11 07:22:21 -0600

    Day 3 start.

---
+++ 2019/aoc_elixir/lib/aoc_elixir_3.ex
@@ -0,0 +1,14 @@
+defmodule AocElixir3 do
+  @moduledoc """
+  Documentation for AocElixir3.
+  """
+
+
+  def createGrid(wires) do
+    for wire <- wires do
+      IO.inspect(wire)
+    end
+
+  end
+
+end
