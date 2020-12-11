67a903daddf4b15e478f8a2a470b1144f82832c9
Andy Glassman <aglassman@frame.io>
2020-12-10 21:36:50 -0600

    Day 10

--- 2020/aoc_elixir/test/day_09_test.exs
+++ 2020/aoc_elixir/test/day_09_test.exs
@@ -29,7 +29,7 @@ defmodule Day09Test do
   end
 
   test "day 9 - part 1" do
-    assert 1492208709 = find_missing(File.read!("input/9.txt"), 25)
+    assert 1_492_208_709 = find_missing(File.read!("input/9.txt"), 25)
   end
 
   test "example - part 2" do
@@ -37,13 +37,13 @@ defmodule Day09Test do
   end
 
   test "day 9 - part 2" do
-    assert 238243506 = find_weakness(File.read!("input/9.txt"), 1492208709)
+    assert 238_243_506 = find_weakness(File.read!("input/9.txt"), 1_492_208_709)
   end
 
   def find_missing(input, preamble_size) do
     to_int_stream(input, preamble_size + 1)
     |> Stream.map(&Enum.reverse/1)
-    |> Enum.find_value(fn  [sum | remaining] ->
+    |> Enum.find_value(fn [sum | remaining] ->
       case find_sums_2(remaining, sum) do
         :not_found -> sum
         _ -> nil
@@ -77,6 +77,7 @@ defmodule Day09Test do
 
   def find_sums_2(input, sum) do
     input = :lists.usort(input)
+
     try do
       for a <- input, b <- input do
         cond do
@@ -84,6 +85,7 @@ defmodule Day09Test do
           true -> nil
         end
       end
+
       :not_found
     catch
       {:found, answer} -> {:found, answer}
---
+++ 2020/aoc_elixir/test/day_10_test.exs
@@ -0,0 +1,69 @@
+defmodule Day10Test do
+  use ExUnit.Case
+
+  test "example - part 1" do
+    assert 35 = part_1(File.read!("input/10-example.txt"))
+  end
+
+  test "day 10 - part 1" do
+    assert 2112 = part_1(File.read!("input/10.txt"))
+  end
+
+  test "example - part 2" do
+    assert 8 == part_2(File.read!("input/10-example.txt"))
+    assert 19208 == part_2(File.read!("input/10-example-2.txt"))
+  end
+
+  test "day 10 - part 2" do
+    assert 3022415986688 == part_2(File.read!("input/10.txt"))
+  end
+
+  def part_1(input) do
+    %{1 => a, 3 => b} = input
+    |> full_chain()
+    |> jolt_differences()
+    |> jolt_difference_map()
+
+    a * b
+  end
+
+  def part_2(input) do
+    input
+    |> full_chain()
+    |> jolt_differences()
+    |> Enum.chunk_by(&(&1 == 3))
+    |> Enum.reject(&match?([3 | _], &1))
+    |> Enum.map(&calc_combos(&1))
+    |> Enum.reduce(&*/2)
+  end
+
+  def calc_combos(combos) do
+    combos
+    |> Enum.with_index()
+    |> Enum.reduce(1, fn {_, i}, acc -> acc + i end)
+  end
+
+  def full_chain(input) do
+    chain =
+      String.split(input, "\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sort()
+
+    [last] = Enum.take(chain, -1)
+
+    [0] ++ chain ++ [last + 3]
+  end
+
+  def jolt_differences(chain) do
+    chain
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Stream.map(fn [a, b] -> b - a end)
+  end
+
+  def jolt_difference_map(jolt_differences) do
+    Enum.reduce(
+      jolt_differences,
+      %{},
+      &Map.update(&2, &1, 1, fn a -> a + 1 end))
+  end
+end
