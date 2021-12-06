5822b4ba49a2cf0c82cb1d34fcc17a609bad880b
Andrew Glassman <andrew.glassman@gmail.com>
2021-12-05 21:37:57 -0600

    Day 5

--- 2021/aoc_elixir/test/day_04_test.exs
+++ 2021/aoc_elixir/test/day_04_test.exs
@@ -2,7 +2,6 @@ defmodule Day04Test do
   use ExUnit.Case
   use Bitwise
 
-
   test "example" do
     assert 4512 ==
              File.read!("input/4.example.txt")
@@ -42,9 +41,10 @@ defmodule Day04Test do
   def parse_input(input) do
     [sequence_string | board_strings] = String.split(input, "\n\n", trim: true)
 
-    sequence = sequence_string
-    |> String.split(",", trim: true)
-    |> Enum.map(&String.to_integer/1)
+    sequence =
+      sequence_string
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
 
     boards = Enum.map(board_strings, &string_to_board/1)
 
@@ -60,11 +60,12 @@ defmodule Day04Test do
 
   # score one game
   def score({called_numbers, last_called, winning_row, board}) do
-    unmarked_sum = board
-    |> Enum.flat_map(fn x -> x end)
-    |> MapSet.new()
-    |> MapSet.difference(called_numbers)
-    |> Enum.reduce(&+/2)
+    unmarked_sum =
+      board
+      |> Enum.flat_map(fn x -> x end)
+      |> MapSet.new()
+      |> MapSet.difference(called_numbers)
+      |> Enum.reduce(&+/2)
 
     unmarked_sum * last_called
   end
@@ -100,21 +101,29 @@ defmodule Day04Test do
 
   def play_bingo([to_call | remaining_calls], called_numbers, boards, wins) do
     called_numbers = MapSet.put(called_numbers, to_call)
+
     case find_winners(called_numbers, boards, to_call) do
       [] ->
         play_bingo(remaining_calls, called_numbers, boards, wins)
+
       winners = [win | _] ->
         if wins do
-          new_winners = winners
-          |> Enum.map(fn winner ->
-            {called_numbers, winning_numbers, winning_board} = winner
-            win = {called_numbers, to_call, winning_numbers, winning_board} |> IO.inspect(charlists: :as_lists)
-          end)
+          new_winners =
+            winners
+            |> Enum.map(fn winner ->
+              {called_numbers, winning_numbers, winning_board} = winner
+
+              win =
+                {called_numbers, to_call, winning_numbers, winning_board}
+                |> IO.inspect(charlists: :as_lists)
+            end)
 
           winning_boards = Enum.map(winners, fn {_, _, board} -> board end)
 
           # remove board that has won
-          remaining_boards = Enum.reject(boards, fn board -> Enum.any?(winning_boards, fn x -> x == board end) end)
+          remaining_boards =
+            Enum.reject(boards, fn board -> Enum.any?(winning_boards, fn x -> x == board end) end)
+
           play_bingo(remaining_calls, called_numbers, remaining_boards, wins ++ new_winners)
         else
           win
@@ -122,17 +131,18 @@ defmodule Day04Test do
     end
   end
 
-
   def find_winners(called_numbers, boards, to_call) do
-    row_wins = boards
-    |> Enum.map(&horizontal_win(called_numbers, &1, to_call))
-    |> Enum.reject(&is_nil/1)
+    row_wins =
+      boards
+      |> Enum.map(&horizontal_win(called_numbers, &1, to_call))
+      |> Enum.reject(&is_nil/1)
 
-    column_wins = boards
-    |> Enum.map(&vertical_win(called_numbers, &1, to_call))
-    |> Enum.reject(&is_nil/1)
+    column_wins =
+      boards
+      |> Enum.map(&vertical_win(called_numbers, &1, to_call))
+      |> Enum.reject(&is_nil/1)
 
-    row_wins ++ column_wins
+    (row_wins ++ column_wins)
     |> Enum.map(fn winner ->
       {winning_numbers, winning_board} = winner
       {called_numbers, winning_numbers, winning_board}
@@ -146,14 +156,14 @@ defmodule Day04Test do
       |> MapSet.new()
       |> MapSet.subset?(called_numbers)
       |> if do
-           if Enum.any?(row, fn x -> x == to_call end) do
-             {row, board}
-           else
-            nil
-           end
+        if Enum.any?(row, fn x -> x == to_call end) do
+          {row, board}
         else
-            nil
+          nil
         end
+      else
+        nil
+      end
     end)
   end
 
@@ -166,15 +176,14 @@ defmodule Day04Test do
       |> MapSet.new()
       |> MapSet.subset?(called_numbers)
       |> if do
-           if Enum.any?(column |> Tuple.to_list(), fn x -> x == to_call end) do
-             {column  |> Tuple.to_list(), board}
-           else
-             nil
-           end
-         else
-           nil
-         end
+        if Enum.any?(column |> Tuple.to_list(), fn x -> x == to_call end) do
+          {column |> Tuple.to_list(), board}
+        else
+          nil
+        end
+      else
+        nil
+      end
     end)
   end
-
 end
---
+++ 2021/aoc_elixir/test/day_05_test.exs
@@ -0,0 +1,80 @@
+defmodule Day05Test do
+  use ExUnit.Case
+  use Bitwise
+
+  test "example" do
+    assert 5 ==
+             File.read!("input/5.example.txt")
+             |> parse_input()
+             |> chart()
+             |> solve()
+  end
+
+  test "day 04 - part 1" do
+    assert 6283 ==
+             File.read!("input/5.txt")
+             |> parse_input()
+             |> chart()
+             |> solve()
+  end
+
+  test "example 2" do
+    assert 12 ==
+             File.read!("input/5.example.txt")
+             |> parse_input()
+             |> chart(true)
+             |> solve()
+  end
+
+  test "day 04 - part 2" do
+    assert 18864 ==
+             File.read!("input/5.txt")
+             |> parse_input()
+             |> chart(true)
+             |> solve()
+  end
+
+  @doc """
+  returns:
+  [{{x1, y1}, {x2, y2}}, ...]
+  """
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&Regex.run(~r/([\d]+),([\d]+) -> ([\d]+),([\d]+)/, &1))
+    |> Enum.map(fn [_, x1, y1, x2, y2] -> {{i(x1), i(y1)}, {i(x2), i(y2)}} end)
+  end
+
+  def i(string), do: String.to_integer(string)
+
+  @doc """
+  Take the lines as inputs, and put them into a chart of coordinates.
+  If no line exists in a particular coordinate, then there will be no
+  entry.
+  """
+  def chart(lines, consider_diagonal \\ false, chart \\ %{}) do
+    Enum.reduce(lines, chart, fn {{x1, y1}, {x2, y2}} = line, chart ->
+      cond do
+        x1 == x2 || y1 == y2 ->
+          for x <- x1..x2, y <- y1..y2, do: {{x, y}, 1}
+        consider_diagonal ->
+          len = abs(x1 - x2)
+          x_dir = (x2 - x1) / len |> trunc
+          y_dir = (y2 - y1) / len |> trunc
+          for i <- 0..len do
+            {{x1 + (i * x_dir), y1 + (i * y_dir)}, 1}
+          end
+        true ->
+          []
+      end
+      |> Map.new()
+      |> Map.merge(chart, fn _, a, b -> a + b end)
+    end)
+  end
+
+  def solve(chart) do
+    chart
+    |> Enum.reject(fn {k, v} -> v < 2 end)
+    |> Enum.count()
+  end
+end
