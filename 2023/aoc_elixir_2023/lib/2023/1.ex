108ee7ce61efd4623e47610e3a38fc6771d8f8c0
aglassman <andrew.glassman@gmail.com>
2023-12-01 14:48:57 -0600

    Cleaned up solution.

--- 2023/aoc_elixir_2023/lib/2023/1.ex
+++ 2023/aoc_elixir_2023/lib/2023/1.ex
@@ -9,9 +9,8 @@ aoc 2023, 1 do
     end
   end
 
-  def p1(input) do
-    input
-    |> String.split("\n")
+  def identify_and_sum(lines) do
+    lines
     |> Enum.map(&String.graphemes/1)
     |> Enum.reduce(0, fn line, sum ->
       first = Enum.find(line, &is_int/1)
@@ -19,40 +18,28 @@ aoc 2023, 1 do
       {int, _} = Integer.parse(first <> last)
       sum + int
     end)
+  end
+
+  def p1(input) do
+    input
+    |> String.split("\n")
+    |> identify_and_sum()
   end
 
   def p2(input) do
     input
     |> String.split("\n")
     |> Enum.map(&replace/1)
-    |> Enum.map(&String.graphemes/1)
-    |> Enum.reduce(0, fn line, sum ->
-      first = Enum.find(line, &is_int/1)
-      last = line |> Enum.reverse() |> Enum.find(&is_int/1)
-      {int, _} = Integer.parse(first <> last)
-      sum + int
-    end)
+    |> identify_and_sum()
   end
 
-  @replacements [
-            {"one", "1"},
-            {"two", "2"},
-            {"three", "3"},
-            {"four", "4"},
-            {"five", "5"},
-            {"six", "6"},
-            {"seven", "7"},
-            {"eight", "8"},
-            {"nine", "9"},
-          ] |> Map.new()
-
+  @replacements %{"one" => "1", "two" => "2", "three" => "3", "four" => "4", "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine" => "9"}
   @regex ~r/one|two|three|four|five|six|seven|eight|nine/
 
   def replace(line) do
-    new_line = Regex.replace(@regex, line, fn a -> Map.get(@replacements, a) end)
-    cond do
-      new_line == line -> line
-      true -> replace(new_line)
+    case Regex.replace(@regex, line, fn a -> Map.get(@replacements, a) end) do
+      new_line = ^line -> line
+      new_line -> replace(new_line)
     end
   end
 end
