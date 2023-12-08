8bbdfa7880f5d56251b133553626a9646857ffd2
aglassman <andrew.glassman@gmail.com>
2023-12-08 00:37:31 -0600

    clean up day 8

--- 2023/aoc_elixir_2023/lib/2023/8.ex
+++ 2023/aoc_elixir_2023/lib/2023/8.ex
@@ -43,24 +43,19 @@ aoc 2023, 8 do
   def p2(input) do
     {pattern, map} = parse(input)
 
-    starts = map |> Map.keys() |> Enum.filter(fn <<_::binary-size(2)>> <> x -> x == "A" end)
-    ends = map |> Map.keys() |> Enum.filter(fn <<_::binary-size(2)>> <> x -> x == "Z" end) |> MapSet.new()
-
-    nums = for start <- starts do
+    nums = for <<_::binary-size(2)>> <> "A" = start <- Map.keys(map) do
       pattern
       |> Stream.cycle()
-      |> Enum.reduce_while({[start], 0, 0, 0}, fn direction, {positions, step, iterations, prev} ->
-        new_positions = Enum.map(positions, fn position -> map[position][direction] end)
-        case new_positions == [start] || Enum.all?(new_positions, &MapSet.member?(ends, &1)) do
-          true ->
+      |> Enum.reduce_while({start, 0}, fn direction, {position, step} ->
+        case map[position][direction] do
+          <<_::binary-size(2)>> <> "Z" ->
             {:halt, step + 1}
-          false ->
-            {:cont, {new_positions, step + 1, iterations, prev}}
+          new_position ->
+            {:cont, {new_position, step + 1}}
         end
       end)
     end
 
-    nums |> Enum.reduce(fn a, b ->  trunc(a * b / Integer.gcd(a, b)) end)
-
+    Enum.reduce(nums, fn a, b ->  trunc(a * b / Integer.gcd(a, b)) end)
   end
 end
