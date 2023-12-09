60e8a6df93c28858413dfdb170722555e0cc5454
aglassman <andrew.glassman@gmail.com>
2023-12-09 13:11:12 -0600

    Day 9 parts 1 and two.

---
+++ 2023/aoc_elixir_2023/lib/2023/9.ex
@@ -0,0 +1,55 @@
+import AOC
+
+aoc 2023, 9 do
+  @moduledoc """
+  https://adventofcode.com/2023/day/9
+  """
+
+  def decompose(sequence, history) do
+    reduced = sequence
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [a, b] -> b - a end)
+
+    if Enum.all?(reduced, &(&1 == 0)) do
+      [sequence | history]
+    else
+      decompose(reduced, [sequence | history])
+    end
+  end
+
+  def predict([], diff), do: diff
+
+  def predict([sequence | decomp], diff) do
+    [seq_end] = Enum.take(sequence, -1)
+    predict(decomp, seq_end + diff)
+  end
+
+  def diff([_], diffs), do: diffs
+
+  def diff([a, b | rem], diffs), do: diff([b | rem], [b - a | diffs])
+
+  @doc """
+      iex> p1(example_input())
+  """
+  def p1(input) do
+    input
+    |> String.split("\n")
+    |> Stream.map(&(&1 |> String.split(" ") |> Enum.map(fn x -> String.to_integer(x) end)))
+    |> Stream.map(fn sequence -> decompose(sequence, []) end)
+    |> Stream.map(fn decomp -> predict(decomp, 0) end)
+    |> Enum.sum()
+  end
+
+  @doc """
+      iex> p2(example_input())
+  """
+  def p2(input) do
+    input
+    |> String.split("\n")
+    |> Stream.map(&(&1 |> String.split(" ") |> Enum.map(fn x -> String.to_integer(x) end)))
+    |> Stream.map(&Enum.reverse/1)
+    |> Stream.map(fn sequence -> decompose(sequence, []) end)
+    |> Stream.map(fn decomp -> predict(decomp, 0) end)
+    |> Enum.sum()
+  end
+end
