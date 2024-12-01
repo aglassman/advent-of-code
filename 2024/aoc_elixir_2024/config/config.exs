d2bbabedf62a1365f45946a32dc05a1d81188a2e
aglassman <andrew.glassman@gmail.com>
2024-12-01 13:24:54 -0600

    code cleanup

--- 2024/aoc_elixir_2024/config/config.exs
+++ 2024/aoc_elixir_2024/config/config.exs
@@ -2,6 +2,7 @@ import Config
 
 config :iex, inspect: [charlists: :as_lists]
 config :advent_of_code_utils, time_zone: :aoc
+
 config :advent_of_code_utils,
   gen_tests?: true,
   session:
--- 2024/aoc_elixir_2024/lib/2024/1.ex
+++ 2024/aoc_elixir_2024/lib/2024/1.ex
@@ -5,42 +5,42 @@ aoc 2024, 1 do
   https://adventofcode.com/2024/day/1
   """
 
+  def parse(input) do
+    input
+    |> String.split("\n")
+    |> Stream.map(fn line ->
+      line
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Stream.zip()
+    |> Stream.map(&Tuple.to_list/1)
+  end
+
   @doc """
       iex> p1(example_string())
   """
   def p1(input) do
     input
-    |> String.split("\n")
-    |> Stream.map(fn line ->
-      line
-      |> String.split("   ")
-      |> Enum.map(&String.to_integer/1)
-    end)
-    |> Stream.zip()
-    |> Stream.map(&Tuple.to_list/1)
+    |> parse()
     |> Stream.map(&Enum.sort/1)
     |> Stream.zip()
-    |> Enum.reduce(0, fn {a, b}, sum -> sum + abs(b - a) end)
+    |> Enum.reduce(0, fn {l, r}, sum -> sum + abs(l - r) end)
   end
 
   @doc """
       iex> p2(example_string())
   """
   def p2(input) do
-    [list_a, list_b] = input
-    |> String.split("\n")
-    |> Stream.map(fn line ->
-      line
-      |> String.split("   ")
-      |> Enum.map(&String.to_integer/1)
-    end)
-    |> Stream.zip()
-    |> Enum.map(&Tuple.to_list/1)
+    [left, right] =
+      input
+      |> parse()
+      |> Enum.to_list()
 
-    list_b_freq = Enum.frequencies(list_b)
+    freq = Enum.frequencies(right)
 
-    Enum.reduce(list_a, 0, fn item_a, sum ->
-      sum + (item_a * (list_b_freq[item_a] || 0))
+    Enum.reduce(left, 0, fn l, sum ->
+      sum + l * (freq[l] || 0)
     end)
   end
 end
